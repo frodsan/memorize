65a4ea2fd29ea761d67b9f8398d278a3ccfce042
Francesco Rodríguez <frodsan@me.com>
2022-02-26 11:34:27 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Francesco Rodriguez on 26/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
